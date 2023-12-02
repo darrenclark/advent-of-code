0659c79e8731a9ac8776a0f74aa1247c3f6f8fd9
Darren Clark <darrenjoelclark@icloud.com>
2023-12-01 23:23:12 -0500

    2023 day 1 part 2

--- 2023/01/calibration/bin/main.ml
+++ 2023/01/calibration/bin/main.ml
@@ -1,19 +1,36 @@
-let non_digits = Str.regexp "[^0-9]"
+let digit_regex = Str.regexp {|\([0-9]\|one\|two\|three\|four\|five\|six\|seven\|eight\|nine\)|}
+
+let to_digit str = match str with
+  "one" -> "1"
+  | "two" -> "2"
+  | "three" -> "3"
+  | "four" -> "4"
+  | "five" -> "5"
+  | "six" -> "6"
+  | "seven" -> "7"
+  | "eight" -> "8"
+  | "nine" -> "9"
+  | s -> s
+
+let first_digit line =
+  let _ = Str.search_forward digit_regex line 0 in
+    line |> Str.matched_string |> to_digit
+
+let last_digit line =
+  let _ = Str.search_backward digit_regex line (String.length line) in
+    line |> Str.matched_string |> to_digit
 
 let calibration_value line =
-  let digits = Str.global_replace non_digits "" line in
-  let len = String.length digits in
-  let first = String.sub digits 0 1 in
-  let last = String.sub digits (len - 1) 1 in
-    int_of_string (first ^ last)
+  let first = first_digit line in
+  let last = last_digit line in
+  int_of_string (first ^ last)
 
 let () =
   let rec loop acc =
     try
       let line = read_line () in
       let v = calibration_value line in
-        loop acc + v
-    with
-      End_of_file -> acc
+      loop acc + v
+    with End_of_file -> acc
   in
-    Printf.printf "Sum of calibration values: %d\n" (loop 0)
+  Printf.printf "Sum of calibration values: %d\n" (loop 0)
