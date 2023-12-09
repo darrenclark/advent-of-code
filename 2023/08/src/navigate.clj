d8e34c9922641f8bd530a0aa532022db11438544
Darren Clark <darrenjoelclark@icloud.com>
2023-12-08 23:13:03 -0500

    2023 day 8 pt1 attempt 1

--- 2023/08/src/navigate.clj
+++ 2023/08/src/navigate.clj
@@ -19,15 +19,40 @@
 
 (defn count-until-zzz [{:keys [path nav-map]}]
   (reduce
-    (fn [[curr n] dir]
-      (if (= curr "ZZZ")
-        (reduced n)
-        [(left-or-right dir (nav-map curr)) (+ n 1)]))
-    ["AAA" 0]
-    (cycle path)))
+   (fn [[curr n] dir]
+     (if (= curr "ZZZ")
+       (reduced n)
+       [(left-or-right dir (nav-map curr)) (+ n 1)]))
+   ["AAA" 0]
+   (cycle path)))
 
 (defn part1 [opts]
   {:pre [(contains? opts :input)]}
   (let [parsed (parse (opts :input))
         count (count-until-zzz parsed)]
+    (println count)))
+
+(defn all-end-z [nodes]
+  (every? #(str/ends-with? %1 "Z") nodes))
+
+(defn start-nodes [nav-map]
+  (vec
+   (filter #(str/ends-with? %1 "A") (keys nav-map))))
+
+(defn all-left-or-right [side nav-map nodes]
+  (mapv #(left-or-right side (nav-map %1)) nodes))
+
+(defn count-until-all-end-z [{:keys [path nav-map]}]
+  (reduce
+   (fn [[curr n] dir]
+     (if (all-end-z curr)
+       (reduced n)
+       [(all-left-or-right dir nav-map curr) (+ n 1)]))
+   [(start-nodes nav-map) 0]
+   (cycle path)))
+
+(defn part2 [opts]
+  {:pre [(contains? opts :input)]}
+  (let [parsed (parse (opts :input))
+        count (count-until-all-end-z parsed)]
     (println count)))
