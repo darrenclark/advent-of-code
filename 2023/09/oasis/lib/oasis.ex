90f3508ef5fbee5b52d3d767f4869524601856d3
Darren Clark <darrenjoelclark@icloud.com>
2023-12-09 01:18:09 -0500

    2023 day 9 part 2

--- 2023/09/oasis/lib/oasis.ex
+++ 2023/09/oasis/lib/oasis.ex
@@ -4,17 +4,36 @@ defmodule Oasis do
     |> String.split("\n")
     |> Enum.map(&String.split(&1, " "))
     |> Enum.map(&Enum.map(&1, fn x -> String.to_integer(x) end))
-    |> Enum.map(&predict_next/1)
+    |> Enum.map(&extrapolate_forwards/1)
     |> Enum.sum()
     |> IO.inspect(label: "Sum of extrapolated values")
   end
 
-  defp predict_next(history) do
+  def part2(path) do
+    File.read!(path)
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, " "))
+    |> Enum.map(&Enum.map(&1, fn x -> String.to_integer(x) end))
+    |> Enum.map(&extrapolate_backwards/1)
+    |> Enum.sum()
+    |> IO.inspect(label: "Sum of extrapolated values")
+  end
+
+  defp extrapolate_forwards(history) do
     history
     |> Enum.reverse()
     |> expand()
     |> Enum.reverse()
-    |> predict()
+    |> predict_next()
+  end
+
+  defp extrapolate_backwards(history) do
+    history
+    |> Enum.reverse()
+    |> expand()
+    |> Enum.reverse()
+    |> Enum.map(&Enum.reverse/1)
+    |> predict_prev()
   end
 
   defp expand(history) do
@@ -38,15 +57,27 @@ defmodule Oasis do
 
   defp differences(_, acc), do: Enum.reverse(acc)
 
-  defp predict([bottom | rest]), do: do_predict([0 | bottom], rest)
+  defp predict_next([bottom | rest]), do: do_predict_next([0 | bottom], rest)
 
-  defp do_predict(bottom, rest)
+  defp do_predict_next(bottom, rest)
 
-  defp do_predict([a |_], [[b | _]]) do
+  defp do_predict_next([a |_], [[b | _]]) do
     a + b
   end
 
-  defp do_predict([a |_], [[b | _] = new_bottom | rest]) do
-    do_predict([a + b | new_bottom], rest)
+  defp do_predict_next([a |_], [[b | _] = new_bottom | rest]) do
+    do_predict_next([a + b | new_bottom], rest)
+  end
+
+  defp predict_prev([bottom | rest]), do: do_predict_prev([0 | bottom], rest)
+
+  defp do_predict_prev(bottom, rest)
+
+  defp do_predict_prev([a |_], [[b | _]]) do
+    b - a
+  end
+
+  defp do_predict_prev([a |_], [[b | _] = new_bottom | rest]) do
+    do_predict_prev([b - a | new_bottom], rest)
   end
 end
