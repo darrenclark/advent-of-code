d25b011d8c3472f6958047aeb48bb022a2326619
Darren Clark <darren.clark@thescore.com>
2023-12-18 14:23:12 -0500

    2023 day 13 part 2 (TODO: fix)

--- 2023/13/reflection.js
+++ 2023/13/reflection.js
@@ -13,6 +13,32 @@ function findVerticalReflection(pattern) {
   return null;
 }
 
+function findReflection(pattern) {
+  let index = findVerticalReflection(pattern);
+  if (index != null) {
+    return {direction: "vertical", index: index};
+  }
+
+  index = findVerticalReflection(rotate(pattern));
+  if (index != null) {
+    return {direction: "horizontal", index: index};
+  }
+
+  return null;
+}
+
+function reflectionEqual(a, b) {
+  return a.index == b.index && a.direction == b.direction;
+}
+
+function reflectionValue(r) {
+  if (r.direction == 'vertical') {
+    return r.index * 100;
+  } else {
+    return r.index;
+  }
+}
+
 function rotate(pattern) {
   let result = [];
 
@@ -27,6 +53,12 @@ function rotate(pattern) {
   return result;
 }
 
+function flip(row, index) {
+  let replacement = row[index] == '#' ? '.' : '#';
+
+  return row.substr(0, index) + replacement + row.substr(index + 1, row.length - 1 - index)
+}
+
 function main() {
   if (process.argv.length != 3) {
     console.error("usage: node reflections.js path/to/input.txt");
@@ -35,19 +67,37 @@ function main() {
 
   let input = fs.readFileSync(process.argv[2], {encoding: 'utf8', mode: 'r'});
 
-  let sum = 0;
+  let sumPart1 = 0;
+  let sumPart2 = 0;
 
   for (p of input.split("\n\n")) {
-    var pattern = p.trim().split("\n");
+    let pattern = p.trim().split("\n");
 
-    let vertical = findVerticalReflection(pattern)
-    if (vertical != null) {
-      sum += vertical * 100;
-    } else {
-      sum += findVerticalReflection(rotate(pattern));
+    // Part 1
+    let reflection = findReflection(pattern);
+    sumPart1 += reflectionValue(reflection);
+
+    // Part 2
+    let foundSecondReflection = false;
+    for (var y = 0; y < pattern.length; y++) {
+      for (var x = 0; x < pattern[y].length; x++) {
+        let original = pattern[y];
+        pattern[y] = flip(original, x);
+
+        let reflection2 = findReflection(pattern);
+        if (reflection2 != null && !reflectionEqual(reflection, reflection2)) {
+          sumPart2 += reflectionValue(reflection2);
+          foundSecondReflection = true;
+          break;
+        }
+
+        pattern[y] = original;
+      }
+      if (foundSecondReflection) break;
     }
   }
-  console.log("[PART 1] Summarized pattern notes:", sum);
+  console.log("[PART 1] Summarized pattern notes:", sumPart1);
+  console.log("[PART 2] Summarized pattern notes:", sumPart2);
 }
 
 main()
