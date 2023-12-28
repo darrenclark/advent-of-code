19f5a38fa323b2b7d088ce3aa06f15b41e33c66a
Darren Clark <darren.clark@thescore.com>
2023-12-27 17:36:19 -0700

    unsuccessfully try to make 2023 day 12 part 2 work

--- 2023/12/main.m
+++ 2023/12/main.m
@@ -1,53 +1,165 @@
 #import <Foundation/Foundation.h>
 
-@interface Solver: NSObject
+static NSCountedSet *counts;
 
-@property (nonatomic, strong) NSString *conditionRecord;
-@property (nonatomic, strong) NSArray<NSNumber *> *contiguousGroups;
+typedef NSArray<NSArray<NSNumber *> *> *PossibleGroups;
+
+@interface Memorize : NSObject
+
+@property(nonatomic, strong) NSMutableDictionary *results;
+
+@end
+
+@implementation Memorize
+
+- (instancetype)init {
+  if (self = [super init]) {
+    _results = [NSMutableDictionary dictionary];
+  }
+  return self;
+}
+
+- (id)lookup:(id)key orCompute:(id (^)())block {
+  id result = _results[key];
+  if (result != nil) {
+    return result;
+  }
+  result = block();
+  _results[key] = result;
+  return result;
+}
+
+@end
+
+@interface Solver : NSObject
+
+@property(nonatomic, strong) NSArray<NSString *> *parts;
+@property(nonatomic, strong) NSArray<NSNumber *> *contiguousGroups;
+@property(nonatomic, strong) Memorize *memoizePossibleSolutions;
+@property(nonatomic, strong) Memorize *memoizePossibleGroups;
 
 @end
 
 @implementation Solver
 
-- (instancetype)initWithConditionRecord:(NSString *)conditionRecord contiguousGroups:(NSArray<NSNumber *> *)contiguousGroups {
+- (instancetype)initWithConditionRecord:(NSString *)conditionRecord
+                       contiguousGroups:
+                           (NSArray<NSNumber *> *)contiguousGroups {
   if (self = [super init]) {
-    _conditionRecord = conditionRecord;
     _contiguousGroups = contiguousGroups;
+
+    _memoizePossibleSolutions = [[Memorize alloc] init];
+    _memoizePossibleGroups = [[Memorize alloc] init];
+
+    NSMutableArray *parts = [NSMutableArray array];
+    for (NSString *string in
+         [conditionRecord componentsSeparatedByString:@"."]) {
+      if (string.length > 0) {
+        [parts addObject:string];
+      }
+    }
+
+    _parts = parts;
   }
   return self;
 }
 
-- (int)countPossibleSolutions {
-  NSMutableString *solution = [self.conditionRecord mutableCopy];
-
-  return [self countPossibleSolutions:solution fromIndex:0];
+- (long)countPossibleSolutions {
+  return [self countPossibleSolutions:[NSArray array] fromIndex:0].longValue;
 }
 
-- (int)countPossibleSolutions:(NSMutableString *)solution fromIndex:(NSUInteger)index {
-  if (index == solution.length) {
-    return [self isValid:solution] ? 1 : 0;
-  } else if ([self.conditionRecord characterAtIndex:index] == '?') {
-    int result = 0;
+- (NSNumber *)countPossibleSolutions:(NSArray *)groups
+                           fromIndex:(NSUInteger)index {
+  id key = @[ groups, @(index) ];
+  return [_memoizePossibleSolutions
+         lookup:key
+      orCompute:^id {
+        if (index == _parts.count) {
+          return [groups isEqual:self.contiguousGroups] ? @1 : @0;
+        } else {
+          long result = 0;
 
-    [solution replaceCharactersInRange:NSMakeRange(index, 1) withString:@"."];
-    result += [self countPossibleSolutions:solution fromIndex:index + 1];
+          if (groups.count == _contiguousGroups.count) {
+            // only "no groups" will match
+            for (NSArray *possibleGroups in
+                 [self possibleGroups:_parts[index]]) {
+              if (possibleGroups.count == 0) {
+                result += [self countPossibleSolutions:groups
+                                             fromIndex:index + 1]
+                              .longValue;
+              }
+            }
 
-    [solution replaceCharactersInRange:NSMakeRange(index, 1) withString:@"#"];
-    result += [self countPossibleSolutions:solution fromIndex:index + 1];
+          } else {
+            for (NSArray *possibleGroups in
+                 [self possibleGroups:_parts[index]]) {
+              if (![self nextGroups:possibleGroups
+                      arePossibleAtGroupIndex:groups.count])
+                continue;
 
-    return result;
+              result +=
+                  [self
+                      countPossibleSolutions:
+                          [groups arrayByAddingObjectsFromArray:possibleGroups]
+                                   fromIndex:index + 1]
+                      .longValue;
+            }
+          }
+
+          return @(result);
+        }
+      }];
+}
+
+- (BOOL)nextGroups:(NSArray *)possibleGroups
+    arePossibleAtGroupIndex:(NSUInteger)index {
+  if (index + possibleGroups.count > _contiguousGroups.count)
+    return NO;
+
+  for (NSUInteger i = 0; i < possibleGroups.count; i++) {
+    if (![possibleGroups[i] isEqual:_contiguousGroups[index + i]])
+      return NO;
+  }
+
+  return YES;
+}
+
+- (PossibleGroups)possibleGroups:(NSString *)part {
+  return [_memoizePossibleGroups lookup:part
+                              orCompute:^id {
+                                NSMutableArray *answer = [NSMutableArray array];
+                                [self findPossibleGroups:[part mutableCopy]
+                                               fromIndex:0
+                                              intoAnswer:answer];
+                                return answer;
+                              }];
+}
+
+- (void)findPossibleGroups:(NSMutableString *)part
+                 fromIndex:(NSUInteger)index
+                intoAnswer:(NSMutableArray *)answer {
+  if (index == part.length) {
+    [answer addObject:[self parseGroups:part]];
+  } else if ([part characterAtIndex:index] == '?') {
+    [part replaceCharactersInRange:NSMakeRange(index, 1) withString:@"."];
+    [self findPossibleGroups:part fromIndex:index + 1 intoAnswer:answer];
+
+    [part replaceCharactersInRange:NSMakeRange(index, 1) withString:@"#"];
+    [self findPossibleGroups:part fromIndex:index + 1 intoAnswer:answer];
+
+    [part replaceCharactersInRange:NSMakeRange(index, 1) withString:@"?"];
   } else {
-    return [self countPossibleSolutions:solution fromIndex:index + 1];
+    [self findPossibleGroups:part fromIndex:index + 1 intoAnswer:answer];
   }
 }
 
-- (BOOL)isValid:(NSString *)solution {
-  NSMutableArray *groups = [NSMutableArray arrayWithCapacity:self.contiguousGroups.count];
+- (NSArray *)parseGroups:(NSString *)part {
+  NSMutableArray *groups = [NSMutableArray array];
 
   int currentGroupSize = 0;
 
-  for (NSUInteger i = 0; i < solution.length; i++) {
-    unichar ch = [solution characterAtIndex:i];
+  for (NSUInteger i = 0; i < part.length; i++) {
+    unichar ch = [part characterAtIndex:i];
 
     if (ch == '#') {
       currentGroupSize += 1;
@@ -60,20 +172,30 @@
   if (currentGroupSize > 0)
     [groups addObject:@(currentGroupSize)];
 
-  return [groups isEqual:self.contiguousGroups];
+  return groups;
 }
 
 @end
 
-
 int processLine(NSString *line) {
   NSArray *parts = [line componentsSeparatedByString:@" "];
   NSString *conditions = parts[0];
-  NSArray<NSNumber *> *contiguousGroups = [[parts[1] componentsSeparatedByString:@","] valueForKey:@"intValue"];
+  NSArray<NSNumber *> *contiguousGroups =
+      [[parts[1] componentsSeparatedByString:@","] valueForKey:@"intValue"];
 
-  Solver *solver = [[Solver alloc] initWithConditionRecord:conditions contiguousGroups:contiguousGroups];
+  Solver *solver = [[Solver alloc] initWithConditionRecord:conditions
+                                          contiguousGroups:contiguousGroups];
 
   return [solver countPossibleSolutions];
+}
+
+NSString *unfold5x(NSString *line) {
+  NSArray *parts = [line componentsSeparatedByString:@" "];
+
+  return [NSString stringWithFormat:@"%@?%@?%@?%@?%@ %@,%@,%@,%@,%@", parts[0],
+                                    parts[0], parts[0], parts[0], parts[0],
+                                    parts[1], parts[1], parts[1], parts[1],
+                                    parts[1]];
 }
 
 int main(int argc, char *argv[]) {
@@ -94,15 +216,26 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  int result = 0;
+  long result = 0;
 
   for (NSString *line in [fileContents componentsSeparatedByString:@"\n"]) {
     if (line.length > 0) {
-      result += processLine(line);
+      long r = processLine(line);
+      result += r;
     }
   }
 
-  NSLog(@"[Part 1] Sum of possible solutions: %d", result);
+  NSLog(@"[Part 1] Sum of possible solutions: %ld", result);
+
+  result = 0;
+
+  for (NSString *line in [fileContents componentsSeparatedByString:@"\n"]) {
+    if (line.length > 0) {
+      result += processLine(unfold5x(line));
+    }
+  }
+
+  NSLog(@"[Part 2] Sum of possible solutions: %ld", result);
 
   return 0;
 }
