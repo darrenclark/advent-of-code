b79343af0ad39715e418fa2aca35f1200affaa3d
Darren Clark <darren.clark@thescore.com>
2023-12-30 06:48:47 -0500

    template rest of 2015

---
+++ 2015/09/solution.py
@@ -0,0 +1,16 @@
+input = open('input.txt', 'r').read()
+
+def parse(line):
+    return line
+
+parsed = [parse(line) for line in input.split('\n') if line != ""]
+
+def part1():
+    return 0
+
+print(f'Part 1: {part1()}')
+
+def part2():
+    return 0
+
+print(f'Part 2: {part2()}')
