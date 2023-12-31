2341140fb83aef5cb33339dd37894afa65568e05
Darren Clark <darren.clark@thescore.com>
2023-12-31 18:12:29 -0500

    2015 day 12

--- 2015/12/solution.py
+++ 2015/12/solution.py
@@ -1,16 +1,37 @@
+import json
+
 input = open('input.txt', 'r').read()
 
-def parse(line):
-    return line
-
-parsed = [parse(line) for line in input.split('\n') if line != ""]
+parsed = json.loads(input)
 
 def part1():
-    return 0
+    def sum_json(obj):
+        if type(obj) == int:
+            return obj
+        elif type(obj) == list:
+            return sum([sum_json(x) for x in obj])
+        elif type(obj) == dict:
+            return sum([sum_json(x) for x in obj.values()])
+        else:
+            return 0
+
+    return sum_json(parsed)
 
 print(f'Part 1: {part1()}')
 
 def part2():
-    return 0
+    def sum_json(obj):
+        if type(obj) == int:
+            return obj
+        elif type(obj) == list:
+            return sum([sum_json(x) for x in obj])
+        elif type(obj) == dict:
+            if 'red' in obj.values():
+                return 0
+            return sum([sum_json(x) for x in obj.values()])
+        else:
+            return 0
+
+    return sum_json(parsed)
 
 print(f'Part 2: {part2()}')
