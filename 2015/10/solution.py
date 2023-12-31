d9e8371e5496ca2d81dd7bb7b5425c22e75f441b
Darren Clark <darren.clark@thescore.com>
2023-12-31 17:40:34 -0500

    2015 day 10

--- 2015/10/solution.py
+++ 2015/10/solution.py
@@ -1,16 +1,35 @@
 input = open('input.txt', 'r').read()
 
-def parse(line):
-    return line
+parsed = input.split('\n')[0]
 
-parsed = [parse(line) for line in input.split('\n') if line != ""]
+def look_and_say(s):
+    res = ""
+    curr = s[0]
+    n_curr = 1
+    for c in s[1:]:
+        if c != curr:
+            res += str(n_curr) + curr
+            curr = c
+            n_curr = 1
+        else:
+            n_curr += 1
+
+    res += str(n_curr) + curr
+    return res
+
+def look_and_say_n(s, n):
+    res = s
+    for _ in range(0, n):
+        res = look_and_say(res)
+    return res
+
 
 def part1():
-    return 0
+    return len(look_and_say_n(parsed, 40))
 
 print(f'Part 1: {part1()}')
 
 def part2():
-    return 0
+    return len(look_and_say_n(parsed, 50))
 
 print(f'Part 2: {part2()}')
