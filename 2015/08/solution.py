f7c53592b38921851af2a7d809f58484de9083d1
Darren Clark <darren.clark@thescore.com>
2023-12-31 17:21:54 -0500

    2015 day 8

--- 2015/08/solution.py
+++ 2015/08/solution.py
@@ -1,16 +1,30 @@
 input = open('input.txt', 'r').read()
 
 def parse(line):
-    return line
+    return (line, eval(line))
 
 parsed = [parse(line) for line in input.split('\n') if line != ""]
 
 def part1():
-    return 0
+    in_code = sum([len(x[0]) for x in parsed])
+    in_mem = sum([len(x[1]) for x in parsed])
+    return in_code - in_mem
 
 print(f'Part 1: {part1()}')
 
 def part2():
-    return 0
+    def encode(s):
+        out = ""
+        for c in s:
+            if c in "\\\"":
+                out += "\\" + c
+            else:
+                out += c
+
+        return "\"" + out + "\""
+
+    encoded = sum([len(encode(x[0])) for x in parsed])
+    in_code = sum([len(x[0]) for x in parsed])
+    return encoded - in_code
 
 print(f'Part 2: {part2()}')
